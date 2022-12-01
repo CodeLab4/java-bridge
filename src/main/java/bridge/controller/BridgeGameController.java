efcac9b3c7af80fde838d6259fb415c7061f3de0
lvalentine6 <ysr1227@naver.com>
2022-12-01 18:37:00 +0900

    feat : BridgeGameController 구조 구현

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,4 +1,33 @@
 package bridge.controller;
 
+import bridge.model.BridgeGame;
+import bridge.model.BridgeMaker;
+import bridge.model.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class BridgeGameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final BridgeGame bridgeGame;
+    private final BridgeMaker bridgeMaker;
+
+    public BridgeGameController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.bridgeGame = new BridgeGame();
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    }
+
+    public void gameStart() {
+
+    }
+
+    public void gameProgress() {
+
+    }
+
+    public void gameRetry() {
+
+    }
 }
