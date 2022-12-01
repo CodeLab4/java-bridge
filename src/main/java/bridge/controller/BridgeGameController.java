55713ce35f1b36464adfcf235b4ec3f4d3cb3aab
lvalentine6 <ysr1227@naver.com>
2022-12-01 21:18:37 +0900

    feat : 다리 길이 입력 기능, 예외치리 기능 구현

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -5,6 +5,7 @@ import bridge.model.BridgeMaker;
 import bridge.model.BridgeRandomNumberGenerator;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+import bridge.view.OutputView.Message;
 
 public class BridgeGameController {
     private final InputView inputView;
@@ -20,7 +21,20 @@ public class BridgeGameController {
     }
 
     public void gameStart() {
+        Message.GAME_START_MESSAGE.infoMessage();
+        Message.GAME_BRIDGE_LENGTH_INPUT_MESSAGE.infoMessage();
+        bridgeSizeChecker();
+    }
 
+    public void bridgeSizeChecker() {
+        boolean flag = true;
+        while (flag) {
+            String input = inputView.readBridgeSize();
+            if(!(validNumber(input)||validNumberRange(input))) {
+                flag = false;
+            }
+        }
+        gameProgress();
     }
 
     public void gameProgress() {
@@ -29,5 +43,50 @@ public class BridgeGameController {
 
     public void gameRetry() {
 
+    }
+
+    public boolean validNumber(String input) {
+        String compareString = input.replaceAll("[0-9]", "");
+        try {
+            if(compareString.length() != 0) {
+                throw new IllegalArgumentException(Message.ERROR_INPUT_NUMBER_MESSAGE.errorMessage());
+            }
+        } catch (IllegalArgumentException illegalArgumentException) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean validNumberRange(String input) {
+        try {
+            if(Integer.parseInt(input) < 3 || Integer.parseInt(input) > 20) {
+                throw new IllegalArgumentException(Message.ERROR_INPUT_RANGE_MESSAGE.errorMessage());
+            }
+        } catch (IllegalArgumentException illegalArgumentException) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean validUpDown(String input) {
+        try {
+            if(!(input.equals("U") || input.equals("D"))) {
+                throw new IllegalArgumentException(Message.ERROR_INPUT_UP_DOWN_MESSAGE.errorMessage());
+            }
+        } catch (IllegalArgumentException illegalArgumentException) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean validRetry(String input) {
+        try {
+            if(!(input.equals("R") || input.equals("Q"))) {
+                throw new IllegalArgumentException(Message.ERROR_INPUT_RETRY_MESSAGE.errorMessage());
+            }
+        } catch (IllegalArgumentException illegalArgumentException) {
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -4,6 +4,37 @@ package bridge.view;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    public enum Message {
+        GAME_START_MESSAGE("다리 건너기 게임을 시작합니다. \n"),
+        GAME_BRIDGE_LENGTH_INPUT_MESSAGE("다리의 길이를 입력해주세요."),
+        GAME_PROGRESS_MOVE_POSITION_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+        GAME_RETRY_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+        GAME_RESULT_HEADER_MESSAGE("최종 게임 결과"),
+        GAME_RESULT_BODY_FLAG_MESSAGE("게임 성공 여부: "),
+        GAME_RESULT_BODY_COUNT_MESSAGE("총 시도한 횟수: "),
+        ERROR_INPUT_RANGE_MESSAGE("다리 길이는 3부터 20 사이의 숫자여야 합니다."),
+        ERROR_INPUT_NUMBER_MESSAGE("숫자만 입력해 주세요."),
+        ERROR_INPUT_UP_DOWN_MESSAGE("U, D만 입력홰 주세요."),
+        ERROR_INPUT_RETRY_MESSAGE("R, Q만 입력해 주세요.");
+        private final String message;
+
+        Message(String message) {
+            this.message = message;
+        }
+
+        public String errorMessage() {
+            System.out.println("[ERROR]" + message);
+            return "[Error]" + message;
+        }
+
+        public void infoMessage() {
+            System.out.println(message);
+        }
+    }
+
+    public void printStart() {
+
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
